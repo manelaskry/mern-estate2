e225b6fd36a99848d4cc476e90d4cf2357ba6485
manelaskry <manlaskry8@gmail.com>
2024-04-17 17:29:47 +0200

    project architecture update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Header from './components/Header';
+
+
+export default function App() {
+  return <BrowserRouter>
+  <Header />
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>  
+      <Route path="/sign-up" element={<SignUp/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/profile" element={<Profile/>}></Route>
+    </Routes>
+  </BrowserRouter> 
+}
