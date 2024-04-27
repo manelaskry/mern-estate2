24770d2762df611a95a88b7a1e9666797c40b6f7
manelaskry <manlaskry8@gmail.com>
2024-04-27 15:56:30 +0200

    midd-err

--- server-side/api/index.js
+++ server-side/api/index.js
@@ -19,6 +19,16 @@ app.use(express.json());
 app.use("/api/user",UserRouter);
 app.use("/api/auth",authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+});
+
 
 app.listen(3000, ()=>{
     console.log('server running on port 3000');
--- server-side/controller/auth.controller.js
+++ server-side/controller/auth.controller.js
@@ -9,6 +9,6 @@ export const signup = async(req,res)=>{
         await newUser.save();
         res.status(201).json("user created successfuly");
     } catch(error){
-       res.status(500).json(error.message); 
+       next(error); 
     }
 };
